79cfe8f2236b242812d320a5963555a300542567
Jordan England-Nelson <JordanEnglandNelson@Jordans-MacBook-Air.local>
2017-04-10 08:35:25 -0700

    Adds option to throw an error if an id is not passed into foo function

--- uniqueID_generator.js
+++ uniqueID_generator.js
@@ -7,3 +7,14 @@ function uniqueId() {
 function foo( id = uniqueId() ) {
   // If no ID is passed into the function, uniqueId() is called, which returns a new unique ID and passes that into the function
 }
+
+
+// Another alternative
+
+function required() {
+  throw "Parameter required!";
+}
+
+function foo( id = required() ){
+  // Error is thrown if no ID is passed in
+}
