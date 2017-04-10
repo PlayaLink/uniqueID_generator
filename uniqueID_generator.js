48baa3b387dcf4db44bdecac68f5fdf3d8cba923
Jordan England-Nelson <JordanEnglandNelson@Jordans-MacBook-Air.local>
2017-04-10 08:16:09 -0700

    Creates uniqueID generator function.

---
+++ uniqueID_generator.js
@@ -0,0 +1,9 @@
+function uniqueId() {
+  var uniqueId;
+  //code that generates the uniqueId;
+  return uniqueId;
+}
+
+function foo( id = uniqueId() ) {
+  // If no ID is passed into the function, uniqueId() is called, which returns a new unique ID and passes that into the function
+}
